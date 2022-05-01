388b7349a21b5647e6e0b642c352c19d95a5e5ac
UncleTimFrost <t.romankov@yandex.ru>
2022-05-01 21:06:09 +0300

    o

--- WeatherForecast.py
+++ WeatherForecast.py
@@ -1,20 +1,20 @@
 from pyowm import OWM
 from pyowm.utils.config import get_default_config
-owm =OWM('d7b48cb7dd4703fb34b7626572b70b81')
+owm = OWM('Write your TOKEN from openweather.org')
 
 place = input(' В каком городе/стране?:')
 mgr = owm.weather_manager()
-observation= mgr.weather_at_place(place)
+observation = mgr.weather_at_place(place)
 w = observation.weather
 config_dict = get_default_config()
-config_dict['language']= 'ru'
+config_dict['language'] = 'ru'
 
 t = w.temperature("celsius")
 # температура
-t1= t['temp']
-t2= t ['feels_like']
-t3= t['temp_max']
-t4= t['temp_min']
+t1 = t['temp']
+t2 = t['feels_like']
+t3 = t['temp_max']
+t4 = t['temp_min']
 # ветер
 wind = w.wind()['speed']
 #влажность
@@ -30,6 +30,6 @@ ti = w.reference_time('iso')
 # давление
 pr = w.pressure['press']
 # видимость
-vd= w.visibility_distance
+vd = w.visibility_distance
 
 print(f'В городе {place} температура {t1} °С, ощушается как {t2} °С, максимальная {t3} °С, минимальная {t4} °С\nСкорость ветра {wind}м/с, влажность {humidity}%, облачность {cloudy}%, статус {st}, детальный статус {dt},\n справочное время {ti}, давление {pr}мм. ртутного столба, видимость {vd} метр')
